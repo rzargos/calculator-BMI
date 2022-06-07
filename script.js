fe2a8f1fc116e072dc42aa7129cafd28b4055146
rzargos <raphael.zargos@gmail.com>
2022-06-07 12:06:19 -0300

    project almost done

---
+++ script.js
@@ -0,0 +1,26 @@
+function calcular() {
+    let nome = document.getElementById("nome").value
+    let alt = document.getElementById("alt").value
+    let peso = document.getElementById("peso").value
+    let imc = Number((peso/(alt*alt)).toFixed(2))
+    let res = document.getElementById("res")
+    if (alt === ""){
+        window.alert('Preencha os dados corretamente!')
+    } else {
+        if (imc<18.5) {
+            res.innerHTML = `<b>Atenção ${nome}, seu IMC é de ${imc} e está na classificação de magreza</b>`
+        } else if (imc >= 18.5 && imc <= 24.99) {
+            res.innerHTML = `<b>Parabéns ${nome}, seu IMC é de ${imc} e está na classificação de normalidade</b>`
+        } else if (imc >= 25 && imc <= 29.99) {
+            res.innerHTML = `<b>Atenção ${nome}, seu IMC é de ${imc} e está na classificação de sobrepeso</b>`
+        } else if (imc >= 30 && imc <= 39.99) {
+            res.innerHTML = `<b>Cuidado ${nome}, seu IMC é de ${imc} e está na classificação de obesidade</b>`
+        } else {
+            res.innerHTML = `<b>Procure um médico ${nome}, seu IMC é de ${imc} e está na classificação de obesidade grave</b>`
+        }
+    }
+    let teste = document.getElementById("teste")
+    let image = document.createElement("img")
+    image.src = "teste.jpg"
+    teste.appendChild(image)
+}
